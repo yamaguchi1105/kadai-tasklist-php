7847451f7275f9585a3388fda0c44dc828c2558d
yamaguchi <weareset1105@gmail.com>
2020-03-01 08:16:32 +0000

    Procfile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
     @else
         <div class="center jumbotron">
             <div class="text-center">
-                <h1>Welcome to the Tasklist</h1>
+                <h1>Welcome to the TaskList</h1>
                 {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
